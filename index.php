7966bda13e901bd585cd9e8eb30f7beabec8ddfe
kuma <inataku0121@gmail,com>
2021-01-27 18:47:59 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>レコFES</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+      <nav>
+        <ul>
+          <li class="nav_home">
+              レコＦＥＳ
+          </li>
+          <li>
+            <a href="#">ユーザー登録</a>
+          </li>
+          <li>
+            <a href="about">ログイン</a>
+          </li>
+          <li>
+            <a href="skills">ゲストログイン</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <section class="bg-img">
+      <!-- キャッチコピー -->
+      <div class="title">
+        <div class="t-content">
+          <!-- <div>
+            <span class="t-title">ただ今、転職活動中！</span>
+            <br>
+            <br>
+            学習中言語：JavaScript/PHP/WordPress
+            <br>
+            希望勤務地：東京
+            <br>
+            <span class="t-message">ご興味持っていただいたらご連絡ください！</span>
+          </div> -->
+        </div>
+      </div>
+      <!-- キャッチコピー(終わり) -->
+      </section>
+      <div>
+        <h2>「レコFES」でできること</h2>
+          <ul class="about">
+            <li>
+              <h3>オススメのフェスを知ることができる！</h3> 
+              <p>テストテストテストテストテスト</p>
+            </li>
+            <li>
+              <h3>フェス好きの仲間とつながれる！</h3> 
+              <p>テストテストテストテストテスト</p>
+            </li>
+            <li>
+              <h3>ログイン限定特典あり！</h3> 
+              <p>テストテストテストテストテスト</p>
+            </li>
+          </ul>
+      </div>
+      <div>
+        <h2>直近の口コミ</h2>
+        <ul class="about">
+          <a href="#">
+            <li>
+              <h3>オススメのフェスを知ることができる！</h3> 
+              <p>テストテストテストテストテスト</p>
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <h3>フェス好きの仲間とつながれる！</h3> 
+              <p>テストテストテストテストテスト</p>
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <h3>フェス好きの仲間とつながれる！</h3> 
+              <p>テストテストテストテストテスト</p>
+            </li>
+          </a>
+        </ul>
+      </div>
+</body>
+</html>
+
