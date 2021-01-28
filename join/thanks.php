25bdd3fb64ced897b0832825415c8995539036bc
kuma <inataku0121@gmail,com>
2021-01-28 18:47:18 +0900

    [add]登録完了ページ作成

---
+++ join/thanks.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>レコフェス</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" type="text/css" href="../css/home.css">
+</head>
+<body>
+<header>
+    <nav>
+      <ul>
+        <li class="nav_home">
+            レコＦＥＳ
+        </li>
+        <li>
+          <a href="#">ユーザー登録</a>
+        </li>
+        <li>
+          <a href="about">ログイン</a>
+        </li>
+        <li>
+          <a href="skills">ゲストログイン</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <div class="wrap">
+    <div class="container">
+      <h1>登録しました</h1>
+      <div class="content">
+        <form action="" method="post">
+          <div class="go_login">
+            <input type="submit" value="ログインする" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <footer>
+    ©2021 Reco.FES 
+  </footer>
+</body>
+</html>
