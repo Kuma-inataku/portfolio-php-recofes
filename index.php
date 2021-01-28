3fcabab692c8116efc3e05ce7741993f3b7067b5
kuma <inataku0121@gmail,com>
2021-01-28 15:04:17 +0900

    [add]仮ホーム画面作成

--- index.php
+++ index.php
@@ -4,76 +4,105 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>レコFES</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<header>
-      <nav>
-        <ul>
-          <li class="nav_home">
-              レコＦＥＳ
-          </li>
-          <li>
-            <a href="#">ユーザー登録</a>
-          </li>
-          <li>
-            <a href="about">ログイン</a>
-          </li>
-          <li>
-            <a href="skills">ゲストログイン</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav_home">
+            レコＦＥＳ
+        </li>
+        <li>
+          <a href="#">ユーザー登録</a>
+        </li>
+        <li>
+          <a href="about">ログイン</a>
+        </li>
+        <li>
+          <a href="skills">ゲストログイン</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
     <section class="bg-img">
       <!-- キャッチコピー -->
       <div class="title">
         <div class="t-content">
-
+          
         </div>
       </div>
       <!-- キャッチコピー(終わり) -->
-      </section>
-      <div>
-        <h2>「レコFES」でできること</h2>
-          <ul class="about">
-            <li>
-              <h3>オススメのフェスを知ることができる！</h3> 
-              <p>テストテストテストテストテスト</p>
-            </li>
-            <li>
-              <h3>フェス好きの仲間とつながれる！</h3> 
-              <p>テストテストテストテストテスト</p>
-            </li>
-            <li>
-              <h3>ログイン限定特典あり！</h3> 
-              <p>テストテストテストテストテスト</p>
-            </li>
-          </ul>
+    </section>
+    <div>
+      <h2>「レコFES」でできること</h2>
+      <div class="about">
+        <div class="ab_left">
+          <img src="images/top_image3.jpg" alt="">
+        </div>
+        <ul class="ab_right">
+          <li>
+            <p>オススメのフェスを知ることができる！</p> 
+            <p>テストテストテストテストテスト</p>
+          </li>
+          <li>
+            <p>フェス好きの仲間とつながれる！</p> 
+            <p>テストテストテストテストテスト</p>
+          </li>
+          <li>
+            <p>ログイン限定特典あり！</p> 
+            <p>テストテストテストテストテスト</p>
+          </li>
+        </ul>
+        </div>
       </div>
       <div>
         <h2>直近の口コミ</h2>
-        <ul class="about">
-          <a href="#">
-            <li>
-              <h3>オススメのフェスを知ることができる！</h3> 
-              <p>テストテストテストテストテスト</p>
+        <ul class="recent_reviews">
+          <!-- <a href="#"> -->
+            <li class="card"> 
+              <!-- <section> -->
+              <!-- [PHP]投稿内容持ってくる -->
+              <img class="card-img" src="images/top_image3.jpg" alt="">
+              <div class="card-content">
+                <p class="card-text">サイコーだった！WANIMAいいね！</p>
+              </div>
+              <div class="card-link">
+                  <a href="http://webcreatorbox.com/">もっと見る</a>
+              </div>
+            <!-- </section> -->
             </li>
-          </a>
-          <a href="#">
-            <li>
-              <h3>フェス好きの仲間とつながれる！</h3> 
-              <p>テストテストテストテストテスト</p>
+            <li class="card"> 
+              <!-- <section> -->
+                <!-- [PHP]投稿内容持ってくる -->
+                <img class="card-img" src="images/top_image4.jpg" alt="">
+                <div class="card-content">
+                <p class="card-text">サイコーだった！[Alexandros]いいね！</p>
+              </div>
+              <div class="card-link">
+                <a href="http://webcreatorbox.com/">もっと見る</a>
+              </div>
+            <!-- </section> -->
             </li>
-          </a>
-          <a href="#">
-            <li>
-              <h3>フェス好きの仲間とつながれる！</h3> 
-              <p>テストテストテストテストテスト</p>
+            <li class="card"> 
+            <!-- <section> -->
+              <!-- [PHP]投稿内容持ってくる -->
+              <img class="card-img" src="images/top_image5.jpg" alt="">
+              <div class="card-content">
+                <p class="card-text">サイコーだった！ロケーションいいね！</p>
+              </div>
+              <div class="card-link">
+                <a href="http://webcreatorbox.com/">もっと見る</a>
+              </div>
+                <!-- [PHP]投稿内容持ってくる -->
+            <!-- </section> -->
             </li>
-          </a>
+          <!-- </a> -->
         </ul>
       </div>
+      <footer>
+      ©2021 Reco.FES 
+      </footer>
 </body>
 </html>
 
