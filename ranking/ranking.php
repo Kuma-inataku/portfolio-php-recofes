d6f69fe0c15c16876dfc3111de7cfb6c5e0b95bc
kuma <inataku0121@gmail,com>
2021-01-28 22:17:53 +0900

    [add]ランキング一覧ページ作成 詳細デザインはデータベース連携後

---
+++ ranking/ranking.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>レコFES</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" type="text/css" href="../css/home.css">
+  <link rel="stylesheet" type="text/css" href="../css/ranking.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav_home">
+            レコＦＥＳ
+        </li>
+        <li class="nav_must">
+          <a href="#">他のランキング</a>
+        </li>
+        <li class="nav_must">
+          <a href="about">口コミする</a>
+        </li>
+        <li>
+          <a href="skills">特典</a>
+        </li>
+        <li>
+          <a href="skills"><?php ?>○○さん</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <section class="rank_wrap">
+    <div class="rank"> 
+      <!-- [PHP]投稿内容持ってくる -->
+      <!-- <img class="rank-img" src="<?php ?>" alt=""> -->
+      <div class="rank-content">
+        <h2>オススメフェスランキング</h2>
+        <ol>
+          <li data-rank="1">
+            <span>1位</span>
+            <a href="#">Rock'in Japan<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li data-rank="2">
+            <span>2位</span>
+            <a href="#">SWEET LOVE SHOWER<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li data-rank="3">
+            <span>3位</span>
+            <a href="#">RIZING SUN<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>4位</span>
+            <a href="#">COUNT DOWN JAPAN<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>5位</span>
+            <a href="#">HAJIKETEMAZARE<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>6位</span>
+            <a href="#">イナズマロック<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>6位</span>
+            <a href="#">カミングKOBE<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>7位</span>
+            <a href="#">ツタフェス<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>8位</span>
+            <a href="#">FUJI ROCK<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>9位</span>
+            <a href="#">JAIGA<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>10位</span>
+            <a href="#">YONFES<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>11位</span>
+            <a href="#">YONFES<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>12位</span>
+            <a href="#">YONFES<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>13位</span>
+            <a href="#">YONFES<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>14位</span>
+            <a href="#">YONFES<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+          <li>
+            <span>15位</span>
+            <a href="#">YONFES<?php ?></a>
+            <p>(<?php ?>票)</p>
+          </li>
+        </ol>
+      </div>
+    </div>
+    </section>
+
+      <!-- ranking.cssでデザイン -->
+      <!-- ランキング -->
+      <div class="title">
+        <div class="t-content">
+          
+        </div>
+      </div>
+      <!-- ランキング(終わり) -->
+      <!-- ranking.cssでデザイン -->
+    </section>
+
+
+      <footer>
+      ©2021 Reco.FES 
+      </footer>
+</body>
+</html>
+
