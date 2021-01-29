7acf1b549cb35d8c76c1dffe80737d701be158b8
kuma <inataku0121@gmail,com>
2021-01-29 18:57:24 +0900

    [add]口コミ内容確認ページ作成 close #13 詳細デザインはデータベースとの連携後

---
+++ review/check.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>レコフェス</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" type="text/css" href="../css/home.css">
+</head>
+<body>
+<header>
+    <nav>
+      <ul>
+        <li class="nav_home">
+            レコＦＥＳ
+        </li>
+        <li class="nav_must">
+          <a href="#">他のランキング</a>
+        </li>
+        <li class="nav_must">
+          <a href="about">口コミする</a>
+        </li>
+        <li>
+          <a href="skills">特典</a>
+        </li>
+        <li>
+          <a href="skills"><?php ?>○○さん</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <div class="wrap">
+    <div class="container">
+      <h1>口コミする</h1>
+      <div class="content">
+        <form action="" method="post">
+          <div class="corner">
+            <p class="subtitle">オススメフェス</p>
+            <p><?php ?></p>
+          </div>
+          <div class="corner">
+            <p class="subtitle">思い出の一枚</p>
+            <div>
+              <p><?php ?></p>
+            </div>
+          </div>
+          <div class="corner">
+            <p class="subtitle">オススメの理由</p>
+            <div>
+              <p><?php ?></p>
+            </div>
+          </div>
+          <div class="go_login">
+            <input type="submit" value="口コミ投稿" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <footer>
+    ©2021 Reco.FES 
+  </footer>
+</body>
+</html>
