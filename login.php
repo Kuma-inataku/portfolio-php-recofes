bdfa14d192bd118de38a6804a52f7d0d463fda21
kuma <inataku0121@gmail,com>
2021-01-31 18:18:43 +0900

    [add]ログイン時COOKIEに保存機能追加

--- login.php
+++ login.php
@@ -1,8 +1,17 @@
 <?php
 require('dbconnect.php');
 
+// COOKIEのemailに何か保存されている場合、そのemailを呼び出す状況を$emailに代入
+if($_COOKIE['email'] !== ''){
+  $email = $_COOKIE['email'];
+}
+
 if(!empty($_POST)){
+  // emailとpasswordの入力フォーム(=method属性が"post"であるformタグ内)にどちらも入力がある場合
   if($_POST['email'] !== '' && $_POST['password'] !== ''){
+    // $emailを元々入力した内容($_POST['email'])に代入
+    $email = $_POST['email'];
+    // ＤＢ上のusersテーブルのemailカラムとpasswordカラムの値を選択
     $login = $db->prepare('SELECT * FROM users WHERE email=? AND password=?');
     $login->execute(array(
       $_POST['email'],
@@ -14,6 +23,11 @@ if(!empty($_POST)){
       // SESSIONに値を代入
       $_SESSION['id'] = $user['id'];
       $_SESSION['time'] = time();
+      // チェックボックスがtrue(=valueがon)である時、COOKIEに_POSTで受け取ったメールアドレスを自動で入力
+      if($_POST['save']==='on'){
+        setcookie('email',$_POST['email'], time()+60*60*24*14);
+      }
+      // ranking/home.phpに遷移
       header('Location: ranking/home.php');
       exit();
     }
@@ -67,11 +81,11 @@ if(!empty($_POST)){
             <?php if ($error['login'] === 'blank'): ?>
             <p class="error">メールアドレスとパスワードをご記入ください</p>
             <?php endif;?>
-            <p class="subtitle">メールアドレス<span class="must">必須</span></p>
-            <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
             <?php if ($error['login'] === 'failed'): ?>
               <p class="error">ログインに失敗しました。正しくご記入ください</p>
             <?php endif;?>
+            <p class="subtitle">メールアドレス<span class="must">必須</span></p>
+            <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email,ENT_QUOTES)); ?>" />
             </div>
           </div>
           <div class="corner">
