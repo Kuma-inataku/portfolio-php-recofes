7e2862c74a645cd51ab36becc6375b51b96076ee
kuma <inataku0121@gmail,com>
2021-02-03 11:21:16 +0900

    [update]質問用にコメント追加 プロフィール編集機能実装のため

--- mypage/edit.php
+++ mypage/edit.php
@@ -18,7 +18,43 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
   exit();
 }
 if(!empty($_POST)){
-  $stmt = $db->prepare('UPDATE users SET name=?, sns_twitter=?, sns_instagram=?, fes_count=?, image=?, profile=? WHERE id=? ');
+    //空欄時のエラー表示(ニックネーム)
+    if($_POST['name'] === ''){
+      $error['name'] = 'blank';
+    }
+    //空欄時のエラー表示(フェス回数)
+    if($_POST['fes_count'] === '選択してください'){
+      $error['fes_count'] = 'must_select';
+    }
+    ///jpg/.png/gif以外が選択された時のエラー表示(画像選択)
+    $fileName = $_FILES['image']['name'];
+    if(!empty($fileName)){
+      $ext = substr($fileName,-3);
+      if($ext != 'jpg' && $ext != 'gif' && $ext != 'png'){
+        $error['image']= 'type';
+      }
+    }
+    //アカウントの重複チェック
+    if(empty($error)){
+      //ニックネームの重複チェック
+      $member = $db->prepare('SELECT COUNT(*) AS cnt_name FROM users WHERE name=?');
+      $member->execute(array($_POST['name']));
+      $recordName = $member->fetch();
+      if ($recordName['cnt_name'] > 0){
+        $error['name'] = 'duplicate';
+      }
+    }
+  // エラーが一つも出て得ない場合
+  if(empty($error)){
+    // 画像データを../user_pictureという場所に投函し、かつ画像データを保持
+    $image = date('YmdHis') . $_FILES['image']['name'];
+    move_uploaded_file($_FILES['image']['tmp_name'], '../user_picture/' . $image);
+  }
+
+
+
+  //-----------------今回の質問箇所---------------
+  $stmt = $db->prepare('UPDATE users SET name=?, sns_twitter=?, sns_instagram=?, fes_count=?, image=?, profile=?, modified=NOW() WHERE id=?');
   $stmt->execute(array(
     $user['name'],
     $user['twitter'],
@@ -27,11 +63,21 @@ if(!empty($_POST)){
     $user['image'],
     $user['profile']
   ));
+  //-----------------[END]今回の質問箇所---------------
+  
+
+
+  // エラーデバックコード
+  // var_dump($_SESSION);
+  // var_dump($db->errorInfo()); 
+  // exit();
+  // [END]エラーデバックコード
   
   header('Location: mypage.php');
   exit();
+  }
+
   
-}
 ?>
 
 <!DOCTYPE html>
@@ -76,46 +122,73 @@ if(!empty($_POST)){
           <div class="corner">
             <p class="subtitle">ニックネーム</p>
             <!-- <p class="error">*ニックネームを入力してください</p> -->
-            <div>
+
+
+            <!-- -----------------今回の質問箇所--------------- -->
               <input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($user['name'],ENT_QUOTES)); ?>" />
-            </div>
+            <!-- -----------------[END]今回の質問箇所--------------- -->
+
+
           </div>
           <div class="corner">
             <p class="subtitle">SNS(Twiter)</p>
-            <div>
+
+
+             <!-- -----------------今回の質問箇所--------------- -->
               <input type="text" name="twitter" size="35" maxlength="255" value="<?php print(htmlspecialchars($user['sns_twitter'],ENT_QUOTES)); ?>" />
-            </div>
+             <!-- -----------------[END]今回の質問箇所--------------- -->
+
+
           </div>
           <div class="corner">
             <p class="subtitle">SNS(Instagram)</p>
-            <div>
+
+
+             <!-- -----------------今回の質問箇所-------------- -->
               <input type="text" name="instagram" size="35" maxlength="255" value="<?php print(htmlspecialchars($user['sns_instagram'],ENT_QUOTES)); ?>" />
-            </div>
+             <!-- -----------------[END]今回の質問箇所--------------- -->
+
+
           </div>
           <div class="corner">
             <p class="subtitle">フェスに行った回数</p>
+
+            
+              <!-- -----------------今回の質問箇所--------------- -->
             <select name="fes_count">
               <option value="<?php print(htmlspecialchars($user['fes_count'],ENT_QUOTES)); ?>"><?php print(htmlspecialchars($user['fes_count'],ENT_QUOTES)); ?>回</option>
               <?php for($i=0; $i<=100; $i++): ?>
               <option value="<?php print $i ?>"><?php print $i . '回' ?></option>
               <?php endfor;?>
             </select>
+              <!-- -----------------[END]今回の質問箇所--------------- -->
+
+
           </div>
           <div class="corner">
             <p class="subtitle">プロフィール画像</p>
             <div>
+
+
+            <!-- -----------------今回の質問箇所--------------- -->
               <img src="../user_picture/<?php print(htmlspecialchars($user['image'],ENT_QUOTES)); ?>" alt="プロフィール画像">
             </div>
             <input type="file" name="image" size="35" maxlength="255" value="" />
+            <!-- -----------------[END]今回の質問箇所--------------- -->
+
+
           </div>
           <div class="corner">
             <p class="subtitle">自己紹介</p>
-            <div>
+
+
+             <!-- -----------------今回の質問箇所--------------- -->
             <textarea name="profile" cols="50" rows="10" placeholder=""><?php print(htmlspecialchars($user['profile'],ENT_QUOTES)); ?></textarea>
-            </div>
+              <!-- -----------------[END]今回の質問箇所--------------- -->
+
+
           </div>
           <div class="go_login">
-            <!-- <input type="submit" onClick="location.href='../mypage.php'" value="登録" /> -->
             <input type="submit" value="登録" />
             </div>
         </form>
